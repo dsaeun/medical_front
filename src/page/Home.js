45b24fcb171a115e4c6cb59bc6c0a2dee9ef5883
dsaeun <daeun84366@gmail.com>
2020-05-22 18:27:24 +0900

    변한거 없성

--- src/page/Home.js
+++ src/page/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
   render(){
     return (
       <div className="contentalign">
-        <h1>this is home</h1>
+        <h1>this is home!!</h1>
       </div>
     );
   }
