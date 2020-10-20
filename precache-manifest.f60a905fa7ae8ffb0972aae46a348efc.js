260a54e45a7bdbc5255435cff02e833f074f420c
kymkim96 <50816889+kymkim96@users.noreply.github.com>
2020-10-20 11:52:39 +0900

    Updates

---
+++ precache-manifest.f60a905fa7ae8ffb0972aae46a348efc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c8915fa14f8ff5700da0cdd611444f",
+    "url": "/medical_front/index.html"
+  },
+  {
+    "revision": "7f1631896bb382faa03e",
+    "url": "/medical_front/static/css/main.221e4c27.chunk.css"
+  },
+  {
+    "revision": "7c7b1b5e9908d5e07704",
+    "url": "/medical_front/static/js/2.23f16124.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/medical_front/static/js/2.23f16124.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f1631896bb382faa03e",
+    "url": "/medical_front/static/js/main.2d1edf76.chunk.js"
+  },
+  {
+    "revision": "949e37d1e0635743ced1",
+    "url": "/medical_front/static/js/runtime-main.ea04d623.js"
+  }
+]);
